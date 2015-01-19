8ba33c620460ff67f1223b87d3b560a22c479b8c
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-01-19 20:50:36 +0100

    Add first part of Clojure tutorial

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,14 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a little teapot!"))
+
+(println "Cleanliness is next to godliness")
+
+(defn train []
+  (println "choo choo!"))
+
+(+ 1 (* 2 3) 4)
