ffa8926d8bf03e80258368e3a878d3d0b9120ffc
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-03-22 22:08:05 +0100

    Fix errors with own hobbit version

--- do-things/myHobbit.clj
+++ do-things/myHobbit.clj
@@ -1,18 +1,31 @@
-(def asym-hobbit-body-parts [{:name "head"} 
-                             {:name "left-eye"} 
-                             {:name "left-ear"}])
+(def asym-hobbit-body-parts [{:name "head" :size 5} 
+                             {:name "left-eye" :size 2} 
+                             {:name "left-ear" :size 1}])
 
-(defn needs-matching-part [part]
-  (re-find #"left-" part))
+(defn needs-matching-part?
+  [part]
+  (re-find #"left-" (:name part)))
 
-(defn symetrize-parts [parts]
-  (loop [[part & rest] parts
-         new-parts []]
-    (if (empty? part)
-      new-parts
-      (recur rest (conj new-parts part)))))
+(defn make-matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-") :size (:size part)})
+
+(defn symetrize-parts 
+  "blabla"
+  [asym-body-parts] 
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts
+            final-body-parts (conj final-body-parts part)]
+        (if (needs-matching-part? part)
+          (recur remaining (conj final-body-parts (make-matching-part part))) 
+          (recur remaining final-body-parts))))))
+
 
 (println (symetrize-parts asym-hobbit-body-parts))
+
 
 
     
