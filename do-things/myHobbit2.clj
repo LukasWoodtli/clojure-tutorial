9e7f92cf6e17033fd40d07bd0a4304edc1cef194
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-03-28 10:09:21 +0100

    Add hobbit body part dict from tutorial

--- do-things/myHobbit2.clj
+++ do-things/myHobbit2.clj
@@ -1,8 +1,24 @@
 
 
-(def asym-parts [{:name "left-arm" :size 3} 
-                 {:name "head" :size 5}
-                 {:name "left-leg" :size 4}])
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
 
 (defn needs-matching-part? [part]
   (re-find #"^left-" (:name part)))
@@ -23,5 +39,5 @@
 
 
 
-(println (create-hobbit asym-parts))
+(println (create-hobbit asym-hobbit-body-parts))
     
